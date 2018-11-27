ae0de6ec02c549267a1d63d6421f6ca31ec8c3d2
cmendible <cmendible@gmail.com>
2018-11-27 10:08:15 +0100

    hello world examples

---
+++ hello-world/csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chsarp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# Developers!!!");
+        }
+    }
+}
