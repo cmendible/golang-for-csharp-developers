b0ab3871eae7b84ec9d4fd8fe642ba7d0b022f8d
cmendible <cmendible@gmail.com>
2018-11-27 10:20:43 +0100

    comments example

---
+++ comments/csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Comments in C#");
+
+            // Single line comment in C#
+
+            /* And this how you do a multiline
+            Comment in C# */
+        }
+    }
+}
---
+++ comments/go/comments.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Comments in Go")
+
+	// Single line comment in GO
+
+	/* And this how you do a multiline
+	Comment in GO */
+}
