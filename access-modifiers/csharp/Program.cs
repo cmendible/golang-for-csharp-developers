1b9c16614f0564e9648743c81b3a4a5b6683d0ea
cmendible <cmendible@gmail.com>
2018-11-27 22:22:15 +0100

    acces modifiers example

---
+++ access-modifiers/csharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Student
+    {
+        // Private variable
+        private string password;
+
+        // Public variable
+        public string Email;
+
+        public Student(string email, string password)
+        {
+            this.password = password;
+            this.Email = email;
+        }
+
+        // Private Function
+        private string InternalPassword()
+        {
+            return password;
+        }
+
+        // Public Function
+        public string GetPassword()
+        {
+            return this.InternalPassword();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Access Modifiers in C#");
+
+            var student = new Student("username@student.io", "password");
+            Console.WriteLine($"email: {student.Email}");
+            Console.WriteLine($"password: {student.GetPassword()}");
+        }
+    }
+}
---
+++ access-modifiers/go/access-modifiers.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Student struct {
+	// Uppercase is public
+	Email string
+	// Lowercase is private
+	password string
+}
+
+// GetPassword is a Public function
+func (u Student) GetPassword() string {
+	return u.getPassword()
+}
+
+// secretPassword is a Private function
+func (u Student) getPassword() string {
+	return u.password
+}
+
+func main() {
+	fmt.Println("Acces Modifiers in Go")
+
+	student := Student{"username@student.io", "password"}
+	fmt.Println("email: " + student.Email)
+	fmt.Println("password: " + student.GetPassword())
+}
