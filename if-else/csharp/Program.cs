2762f44a8e8ae0132e3bb8a0dcd77cf49dbd5c05
cmendible <cmendible@gmail.com>
2018-11-28 10:37:16 +0100

    if-else example

---
+++ if-else/csharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("if else in C#");
+
+            var value = 1;
+            if (value < 0)
+            {
+                Console.WriteLine($"value is negative");
+            }
+            else if (value == 0)
+            {
+                Console.WriteLine($"value is 0");
+            }
+            else
+            {
+                Console.WriteLine($"value is postive");
+            }
+        }
+    }
+}
---
+++ if-else/go/if-else.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("if else in GO")
+
+	value := 1
+	if value < 0 {
+		fmt.Println("value is negative")
+	} else if value == 0 {
+		fmt.Println("value is 0")
+	} else {
+		fmt.Println("value is postive")
+	}
+}
