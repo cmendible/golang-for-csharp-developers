1a4aeb3b7780eeee75bc55052546f9f3601bc181
cmendible <cmendible@gmail.com>
2018-11-29 21:59:48 +0100

    switch example

---
+++ switch/csharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SwithStatement
+{
+    class Program
+    {
+        static void getGenerationName(int yearOfBirth)
+        {
+            switch (yearOfBirth)
+            {
+                case var year when year > 1997:
+                    Console.WriteLine("Post Millenials");
+                    break;
+                case var year when year > 1981 && year <= 1996:
+                    Console.WriteLine("Millenials");
+                    break;
+                case var year when year > 1965 && year <= 1980:
+                    Console.WriteLine("Generation X");
+                    break;
+                case var year when year > 1946 && year <= 1964:
+                    Console.WriteLine("Baby Boomers");
+                    break;
+                default:
+                    Console.WriteLine("Silent generation");
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Switch in C#");
+
+            getGenerationName(1975);
+        }
+    }
+}
---
+++ switch/go/switch.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func getGenerationName(yearOfBirth int) {
+	switch {
+	case yearOfBirth > 1997:
+		fmt.Println("Post Millenials")
+	case yearOfBirth > 1981 && yearOfBirth <= 1996:
+		fmt.Println("Millenials")
+	case yearOfBirth > 1965 && yearOfBirth <= 1980:
+		fmt.Println("Generation X")
+	case yearOfBirth > 1946 && yearOfBirth <= 1964:
+		fmt.Println("Baby Boomers")
+	default:
+		fmt.Println("Silent generation")
+	}
+}
+
+func main() {
+	fmt.Println("Swith in Go")
+
+	getGenerationName(1975)
+}
